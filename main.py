b93a11c4ec131cd9e9e36c5612ef5d0d8a48643c
Ahmad Kamiludin <ahmadkamiludin18@gmail.com>
2023-03-07 21:36:25 +0700

    project done

---
+++ main.py
@@ -0,0 +1,74 @@
+import json
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+        self.quotes_list = []
+
+    def get_quotes(self):
+        res = requests.get(self.url, headers=self.headers)
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            contents = soup.find_all("div", attrs={"class": "quote"})
+            for content in contents:
+                quote = content.find("span", attrs={"class": "text"}).text.strip()
+                author = content.find("small", attrs={"class": "author"}).text.strip()
+                author_detail = content.find("a")["href"]
+                data_dict = {
+                    "quote": quote,
+                    "quotes by": author,
+                    "author detail": self.url + author_detail
+                }
+                self.quotes_list.append(data_dict)
+
+            with open("quotes.json", "w+") as file:
+                json.dump(self.quotes_list, file)
+            print("Berhasil menyimpan data ke dalam file quotes.json")
+
+    def get_detail(self, detail_url):
+        res = requests.get(detail_url, headers=self.headers)
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            author_title = soup.find("h3", attrs={"class": "author-title"}).text.strip()
+            author_born = soup.find("span", attrs={"class": "author-born-date"}).text.strip()
+            author_born_location = soup.find("span", attrs={"class": "author-born-location"}).text.strip()
+            description = soup.find("div", attrs={"class": "author-description"}).text.strip()
+            data_dict = {
+                "author": author_title,
+                "born": author_born,
+                "born location": author_born_location,
+                "description": description
+            }
+            return data_dict
+
+    def generate_format(self, filename, result):
+        df = pandas.DataFrame(result)
+        if ".csv" or ".xlsx" not in filename:
+            df.to_csv(filename + ".csv", index=False)
+            df.to_excel(filename + ".xlsx", index=False)
+        print("DONE!!!")
+
+    def crawling(self):
+        result = []
+        self.get_quotes()
+
+        for quote in self.quotes_list:
+            detail = self.get_detail(quote["author detail"])
+            final_result = {**quote, **detail}
+            result.append(final_result)
+
+        self.generate_format("reports", result)
+
+
+if __name__ == "__main__":
+    url = "https://quotes.toscrape.com"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+    }
+    scraper = Scraper(url, headers)
+    scraper.crawling()
